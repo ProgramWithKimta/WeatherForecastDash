2d87c6d2f1ac98d55c8937fb7c438d4137c5f992
ProgramWithKimta <knguyen4268@gmail.com>
2025-03-16 18:23:32 -0500

    updated server.ts file

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -1,9 +1,29 @@
+import { promises as fs } from 'fs';
+
 // TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: string;
+
+  constructor(name: string, id?:string) {
+    this.id = id || Date.now().toString();
+    this.name = name
+  }
+};
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+private async read() {
+  try {
+    const data = await fs.readFile('searchHistory.json', 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error('Error finding history', err);
+  }
+}
+
+
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
